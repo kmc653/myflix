735fc01aaf187a2856c7daf25ee2fbdb1defefd7
Kevin Chang <changkaimin.big@gmail.com>
2014-11-17 01:05:22 +0800

    controller_test&add_video_reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,5 +14,9 @@ Video.create(title: "monk", description: "monk", small_cover_url: "/tmp/monk.jpg
 Video.create(title: "south_park", description: "south_park", small_cover_url: "/tmp/south_park.jpg", category: comedies)
 Video.create(title: "family_guy", description: "family_guy", small_cover_url: "/tmp/family_guy.jpg", category: comedies)
 Video.create(title: "futurama", description: "futurama", small_cover_url: "/tmp/futurama.jpg", category: comedies)
-Video.create(title: "monk", description: "monk", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg", category: comedies)
+monk = Video.create(title: "monk", description: "monk", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg", category: comedies)
 Video.create(title: "south_park", description: "south_park", small_cover_url: "/tmp/south_park.jpg", category: comedies)
+
+kevin = User.create(full_name: "Kevin Chang", password: "kevin", email: "kevin@example.com")
+Review.create(user: kevin, video: monk, rating: 5, content: "This is a really good movie!")
+Review.create(user: kevin, video: monk, rating: 2, content: "This is a really good movie!")
