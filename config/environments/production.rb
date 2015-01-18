aa64dc1644814d3fa36471d66b9866665c0dbcf8
Kevin Chang <changkaimin.big@gmail.com>
2015-01-18 20:51:40 +0800

    deploy mailgun to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,14 +18,14 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_option = { host: 'infinite-springs-2536.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            'example',
-    password:             '12345',
+    address:              ENV['MAILGUN_SMTP_SERVER'],
+    port:                 ENV['MAILGUN_SMTP_PORT'],
+    domain:               'infinite-springs-2536.herokuapp.com',
+    user_name:            ENV['MAILGUN_SMTP_LOGIN'],
+    password:             ENV['MAILGUN_SMTP_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true  
   }
