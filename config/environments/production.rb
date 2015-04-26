2508eea16e9514dc25197af59f4df5c2e1757fde
Kevin Chang <changkaimin.big@gmail.com>
2015-04-26 16:15:27 +0800

    test stripe-4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Myflix::Application.configure do
   config.assets.compress = true
   config.assets.js_compressor = :uglifier
 
-  config.assets.compile = true
+  config.assets.compile = false
 
   config.assets.digest = true
 
