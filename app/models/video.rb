c59e60134b71f0213f9c4380b9f52ccf4f4ada4b
Kevin Chang <changkaimin.big@gmail.com>
2015-09-26 01:45:49 +0800

    add review filter with elasticsearch

--- app/models/video.rb
+++ app/models/video.rb
@@ -15,8 +15,8 @@ class Video < ActiveRecord::Base
     where("title LIKE ?", "%#{search_term}%").order("created_at DESC")
   end
 
-  def rating
-    reviews.average(:rating).round(1) if reviews.average(:rating)
+  def average_rating
+    reviews.average(:rating).to_f.round(1) if reviews.average(:rating)
   end
 
   def self.search(query, options={})
@@ -34,11 +34,23 @@ class Video < ActiveRecord::Base
       search_definition[:query][:multi_match][:fields] << "reviews.content"
     end
 
+    if options[:rating_from].present? || options[:rating_to].present?
+      search_definition[:filter] = {
+        range: {
+          average_rating: {
+            gte: (options[:rating_from] if options[:rating_from].present?),
+            lte: (options[:rating_to] if options[:rating_to].present?)
+          }
+        }
+      }
+    end
+
     __elasticsearch__.search(search_definition)
   end
 
   def as_indexed_json(options={})
     as_json(
+      methods: [:average_rating],
       only: [:title, :description],
       include: {
         reviews: { only: [:content] }
